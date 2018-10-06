e9f66e02de14c0359bd21a7a2f0da1d6f89e1bf5
Unknown <camilleotillio@gmail.com>
2018-10-06 16:35:39 -0700

    Initialize Project in GitKraken
    Initialized "empty" classes with example images

---
+++ Animals.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Animals here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Animals extends Actor
+{
+    /**
+     * Act - do whatever the Animals wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
